72d1ae2ad27c11a689466683e3fd679115ef5cac
gianni toscano <gianni.toscano@icloud.com>
2025-03-17 17:50:59 +0100

    add5

--- D1.js
+++ D1.js
@@ -86,15 +86,5 @@ let name2 ="John";
 let addConfer=name1 === name2;
 console.log(addConfer);
 
-/*let name3= name2.toLowerCase();
-let name4= name1.toLowerCase();
-
-console.log(name3 === name4);*/
 let risultato=name1.toLowerCase()===name2.toLowerCase();
 console.log(risultato);
-
-/*let lowerCaseString = name2.toLoLowerCase();
-console.log(lowerCaseString);
-
- /*let myString = "HeLLo WoRLD!"
-let lowerCaseString = myString. toLowerCase(); */
